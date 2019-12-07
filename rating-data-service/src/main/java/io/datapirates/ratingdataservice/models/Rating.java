6df9c7e3757ab3cb8bcda95b54004f8f0b803e9b
Akash <kkatriakash@gmail.com>
2019-12-07 16:43:18 +0500

    First Commit

---
+++ rating-data-service/src/main/java/io/datapirates/ratingdataservice/models/Rating.java
@@ -0,0 +1,18 @@
+package io.datapirates.ratingdataservice.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Rating {
+    private String movieId;
+    private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+}
---
+++ rating-data-service/src/test/java/io/datapirates/ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datapirates.ratingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
