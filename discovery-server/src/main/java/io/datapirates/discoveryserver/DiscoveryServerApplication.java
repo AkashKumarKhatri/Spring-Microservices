abdd90c81738fafcdb8402a1967633ab3c7a7faa
Akash <kkatriakash@gmail.com>
2020-07-22 20:44:38 +0500

    test

--- discovery-server/src/main/java/io/datapirates/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/datapirates/discoveryserver/DiscoveryServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-    // test
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
